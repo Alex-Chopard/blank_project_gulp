604c0623c1ac7bd1ca851838c3ef71a4112ffff0
Alex Chopard <alex.chopard1998@gmail.com>
2018-12-06 12:19:30 +0100

    WIP

--- src/js/main.js
+++ src/js/main.js
@@ -1,31 +1,31 @@
+import Vue from 'vue'
+import App from './App'
+// import router from './router'
+// import Bootstrap from './bootstrap'
+import Vuetify from 'vuetify'
 
-import Example from './components/Example'
-import {getBrowser} from './utils/environment'
+import 'vuetify/dist/vuetify.min.css'
+import 'material-components-web/dist/material-components-web.css'
+import colors from 'vuetify/es5/util/colors'
 
-const App = {
-
-    init(){
-        document.addEventListener('DOMContentLoaded', this.ready.bind(this), false);
-    },
-
-    ready(){
-        this.initComponents();
-        this.bindEvent();
-
-        console.log(getBrowser());
-        new Example();
-    },
-
-    bindEvent(){
-
-    },
-
-    initComponents(){
+Vue.use(Vuetify, {
+  theme: {
+    primary: '#3f51b5',
+    secondary: '#b0bec5',
+    accent: '#8c9eff',
+    error: '#b71c1c',
+    matRed: colors.red.accent3
+  }
+})
 
 
-    }
+Vue.config.productionTip = false
+// Vue.use(Bootstrap)
 
-};
-
-
-App.init();
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  // router,
+  template: '<App/>',
+  components: { App }
+})
